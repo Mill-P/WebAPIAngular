b884db511ef314ce267541a5df8067a03d8e78f1
Milan Pasztor <46690092+Mill-P@users.noreply.github.com>
2022-02-06 17:13:08 +0100

    Frontend Changes
    - Items component
    - Item service

--- AngularFrontend/src/app/app-routing.module.ts
+++ AngularFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ItemsComponent } from './items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'items', component:  ItemsComponent},
+  {path: 'items/:id', component: ItemDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WebAPI/Models/Item.cs
+++ WebAPI/Models/Item.cs
@@ -2,5 +2,10 @@
 {
     public class Item
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
     }
 }
